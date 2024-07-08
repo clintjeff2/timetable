bf18be4fb22ed4604a47b4092286e2ba718c28fe
clintjeff2 <clintjeff2@gmail.com>
2024-07-08 03:39:49 +0100

    changed Connection string to atlas, now working

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 
 const URL = 'mongodb://localhost:27017/timetable';
 const URL_DEPLOY =
-	'mongodb+srv://jeff:o1Uf0knhi9bPLpZp@cluster0.b7wyaaw.mongodb.net/timetable?retryWrites=true&w=majority&appName=Cluster0';
+	'mongodb+srv://jeffyouashi:F1mpKo0uUhG5hnD8@cluster0.tx66e35.mongodb.net/timetable';
 mongoose
-	.connect(URL_DEPLOY, {
+	.connect(URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
 	.then(() => console.log('Connected to Database with success!!'))
-	.catch(() => console.log('Failed connecting to database.'));
+	.catch(() => console.log('Failed connecting to database.', err));
 
 app.listen(4000, () => {
 	console.log('Server Started Successfully, running on port 4000');
